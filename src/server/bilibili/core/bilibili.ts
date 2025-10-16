75123d191ecfa198a8b5d9ed64d882ff1a2d6891
lizhensheng <lizhensheng@actionsky.com>
2025-10-16 20:04:36 +0800

    fix: lint

--- src/server/bilibili/core/bilibili.ts
+++ src/server/bilibili/core/bilibili.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import axios from 'axios';
 import { customAlphabet } from 'nanoid';
 import { formatSeconed, filterTitle, sleep } from './utils';
